1fa896ccd84db5dfa4586dd12f511c6c6d30dd84
ehoversten <ehoversten@gmail.com>
2019-02-06 17:04:25 -0500

    connected buttons and labels to respective ViewControllers

---
+++ Compass/CompassViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CompassViewController.swift
+//
+//  Compass
+//
+//  Created by Erik Hoversten on 2/6/19.
+//  Copyright © 2019 Erik Hoversten. All rights reserved.
+//
+
+import UIKit
+
+class CompassViewController: UIViewController {
+
+    @IBAction func northButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func westButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func eastButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func southButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Compass/DirectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DirectionViewController.swift
+//  Compass
+//
+//  Created by Erik Hoversten on 2/6/19.
+//  Copyright © 2019 Erik Hoversten. All rights reserved.
+//
+
+import UIKit
+
+class DirectionViewController: UIViewController {
+
+    @IBOutlet weak var directionLabel: UILabel!
+    
+    @IBAction func dismissButtonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
